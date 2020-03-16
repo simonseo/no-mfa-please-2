4afa662879178291e5e379aac4a2e6fa128ed0ea
Simon Myunggun Seo <ms9144@nyu.edu>
2020-03-17 00:01:27 +0900

    Create register page view

--- mfa_mirror/mfa_mirror/settings.py
+++ mfa_mirror/mfa_mirror/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mfa_mirror.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
