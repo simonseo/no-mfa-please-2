7c9c50de4fc5ae89646467c5778556ddd7a34d65
Simon Seo <simonseo.doubles@gmail.com>
2020-04-17 13:16:11 +0000

    Change settings: static files, allowed hosts

--- mfa_mirror/mfa_mirror/settings.py
+++ mfa_mirror/mfa_mirror/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,10 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Use in production if you want to use `./manage.py collectstatic`
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_generated') # Use in production if you want to use `./manage.py collectstatic`
 
 # Email settings
 EMAIL_USE_TLS = True
