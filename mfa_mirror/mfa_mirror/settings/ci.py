5aaf0874e778f6ed05a13f83a37371d18f0ddc0d
Simon Seo <simonseo.doubles@gmail.com>
2020-04-19 15:30:13 +0900

    format nginx conf (#22)

--- mfa_mirror/mfa_mirror/settings/ci.py
+++ mfa_mirror/mfa_mirror/settings/ci.py
@@ -1,1 +1,13 @@
 from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = [
+    '*'
+]
+TIME_ZONE = 'Asia/Dubai'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
+# Use in production if you want to use `./manage.py collectstatic`
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_generated')
