81fe1ed5c060026e395b7f2f66f1ad050ecec756
Simon Myunggun Seo <ms9144@nyu.edu>
2020-04-19 15:15:53 +0900

    Change production server security settings

--- mfa_mirror/mfa_mirror/settings/production.py
+++ mfa_mirror/mfa_mirror/settings/production.py
@@ -2,20 +2,23 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
-        '.compute.amazonaws.com',
-        '.duo-mfa.online',
-    ]
+    # '.compute.amazonaws.com',
+    '.duo-mfa.online',
+]
 TIME_ZONE = 'Asia/Dubai'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
 # Use in production if you want to use `./manage.py collectstatic`
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_generated') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_generated')
 
 # Session protection from man-in-the-middle or XSS attacks
 # explanation: https://stackoverflow.com/a/28072319/6501783
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
-# SESSION_COOKIE_HTTPONLY = True
-# SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE=True
+SECURE_SSL_REDIRECT = True
+os.environ['HTTPS'] = 'on'
+os.environ['wsgi.url_scheme'] = 'https'
